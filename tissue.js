fcd9ba1e5c3355348a8c43c551bcaf6c4abd0ec7
Aishah <aishah92@gmail.com>
2020-05-03 02:38:39 +0100

    Made captions change with picture

--- tissue.js
+++ tissue.js
@@ -1,23 +1,37 @@
 let imagCarou = ["images/Aishah-cropped.jpg", "images/Rita-Aishah-Omar.jpg", "images/screencast.jpg", "images/group.jpg", "images/medal.jpg", "images/Green-Man-A.jpg", "images/trophy.jpg"];
+let captions = [
+  "March 2019: Securing my place in the ladies final",
+  "February 2020: Securing my place in the ladies final",
+  "February 2017: Womens Team Trophy Final - Universities Pool Council",
+  "February 2018: UCL 2nds Pool Team",
+  "February 2017: Womens Team Trophy Final Silver Medal",
+  "June 2019: CFPL Division 2 Team Winners",
+  "June 2019: CFPL Ladies Individual Championship Trophy"
+]
 let num = 0;
 let checkOn = 0;
 
 function next() {
   let slider = document.getElementById("slider");
+  let cappy = document.getElementById("caption");
   num++;
   if (num >= imagCarou.length) {
     num = 0
   }
   slider.src = imagCarou[num];
+  cappy.innerHTML = captions[num];
 }
 
 function prev() {
   let slider = document.getElementById("slider");
+  let cappy = document.getElementById("caption");
   num--;
   if (num < 0) {
     num = imagCarou.length - 1;
   }
   slider.src = imagCarou[num];
+  cappy.innerHTML = captions[num];
+
 }
 
 function slideshow() {
